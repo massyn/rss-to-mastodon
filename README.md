bacf973ba1c3db73c9e35d7d7d48e9c70dc8a27f
Phil Massyn <phil.massyn@icloud.com>
2024-05-21 06:41:26 +1000

    Update logic

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # rss-to-mastodon
 Publish RSS feeds to Mastodon
+
+## What it is?
+
+This is a simple bot I created to read a number of RSS feeds, and publish the result to a Mastodon [user](https://infosec.exchange/@securityuser).
+
+## How it works
+
+* The script is scheduled via [GitHub Actions](https://docs.github.com/en/actions) and runs every 3 hours.
+* It reads the user's [timeline](https://docs.joinmastodon.org/methods/timelines/#home) via an API call, to determine the last time a status update was performed.
+* Using the [feedparser](https://pypi.org/project/feedparser/) library, a [list of RSS feeds](config.yaml) are read.
+* Any post that is greater than the last time a status update was made, is then posted to Mastodon through an [API call](https://docs.joinmastodon.org/methods/statuses/#create).
+
+
+## Getting started
+
+Environment variables
+
+* `MASTODON_ENDPOINT`
+* `MASTODON_ACCESS_KEY`
+
--- rss-reader.py
+++ rss-reader.py
@@ -11,21 +11,48 @@ TAG_RE = re.compile(r'<[^>]+>')
 def remove_tags(text):
     return TAG_RE.sub('', text)
 
-def post_mastodon(msg,cfg):
-    r = requests.post(
-        f"{cfg['endpoint']}/api/v1/statuses",
-        data={'status': msg},
-        headers={
-            'Authorization': f"Bearer {os.environ['MASTODON_ACCESS_TOKEN']}"
-            }
-        )
-    
-    if r.status_code != 200:
-        print(r.content)
-    else:
-        print("Posted...")
+def mastodon(action,msg = None):
+    if action == 'post':
+        print(f"Posting - {msg}")
+        r = requests.post(
+            f"{os.environ['MASTODON_ENDPOINT']}/api/v1/statuses",
+            data={'status': msg},
+            timeout=30,
+            headers={
+                'Authorization': f"Bearer {os.environ['MASTODON_ACCESS_TOKEN']}"
+                }
+            )
+        
+        if r.status_code != 200:
+            print(r.content)
+        else:
+            print("Posted...")
 
+    if action == 'get':
+        r = requests.get(
+            f"{os.environ['MASTODON_ENDPOINT']}/api/v1/timelines/home",
+            timeout=30,
+            headers={
+                'Authorization': f"Bearer {os.environ['MASTODON_ACCESS_TOKEN']}"
+                }
+            )
+
+        print(r.status_code)
+        if r.status_code >= 200 and r.status_code < 300:
+            return r.json()
+        else:
+            return []
+        
 def main(c):
+    # -- find the latest post
+    x = mastodon('get')
+    if len(x) == 0:
+        # We do this so we can have a timestamp for the next run
+        mastodon('post','Welcome to the RSS Bot')
+        exit(0)
+    
+    latest = dateparser.parse(x[0]['created_at'])
+
     with open(c,'rt') as y:
         cfg = yaml.safe_load(y)
 
@@ -38,9 +65,10 @@ def main(c):
                 feed = {}
 
             for f in feed.get('entries',[]):
-                if (datetime.datetime.now(datetime.timezone.utc) - dateparser.parse(f['published'])) <= datetime.timedelta(hours=1):
+                #if (datetime.datetime.now(datetime.timezone.utc) - dateparser.parse(f['published'])) <= datetime.timedelta(hours=1):
+                if dateparser.parse(f['published']) >= latest:
                     msg = f'''{feed['feed']['title']} - {f['title']})\n\n{remove_tags(f['summary'])}\n\n{f['link']}'''
-                    post_mastodon(msg,cfg['mastodon'])
+                    mastodon('post',msg)
 
 if __name__=='__main__':
     main('config.yaml')
