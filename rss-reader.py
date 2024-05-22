8ee6c9558c81dcbbaa19c89608aa2942a1308b7c
Phil Massyn <phil.massyn@icloud.com>
2024-05-22 20:00:56 +1000

    Times to UTC

--- rss-reader.py
+++ rss-reader.py
@@ -8,12 +8,16 @@ import os
 
 TAG_RE = re.compile(r'<[^>]+>')
 
+def log(txt):
+    ts = datetime.datetime.now(datetime.UTC).strftime('%Y-%m-%d %H:%M:%S')
+    print(f"{ts} - {txt}")
+
 def remove_tags(text):
     return TAG_RE.sub('', text)
 
 def mastodon(action,msg = None):
     if action == 'post':
-        print(f"Posting - {msg}")
+        log(f"Posting - {msg}")
         r = requests.post(
             f"{os.environ['MASTODON_ENDPOINT']}/api/v1/statuses",
             data={'status': msg},
@@ -26,11 +30,12 @@ def mastodon(action,msg = None):
         if r.status_code != 200:
             print(r.content)
         else:
-            print("Posted...")
+            log("Posted...")
 
     if action == 'get':
+        log("Reading Mastodon timeline...")
         r = requests.get(
-            f"{os.environ['MASTODON_ENDPOINT']}/api/v1/timelines/home",
+            f"{os.environ['MASTODON_ENDPOINT']}/api/v1/timelines/home?limit=1",
             timeout=30,
             headers={
                 'Authorization': f"Bearer {os.environ['MASTODON_ACCESS_TOKEN']}"
@@ -50,27 +55,30 @@ def main(c):
         # We do this so we can have a timestamp for the next run
         mastodon('post','Welcome to the RSS Bot')
         exit(0)
-    
+
     latest = dateparser.parse(x[0]['created_at'],settings={'TO_TIMEZONE': 'UTC'})
+    log(f"Latest timestamp from timeline is {latest}")
 
     with open(c,'rt') as y:
         cfg = yaml.safe_load(y)
 
         for rss in cfg['rss']:
-            print(f"rss => {rss}")
+            log(f"rss => {rss}")
 
             try:
                 feed = feedparser.parse(rss, agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
             except:
                 feed = {}
 
-            print(f"- Found {len(feed.get('entries',[]))} items...")
+            log(f"- Found {len(feed.get('entries',[]))} items...")
 
             for f in feed.get('entries',[]):
                 #if (datetime.datetime.now(datetime.timezone.utc) - dateparser.parse(f['published'])) <= datetime.timedelta(hours=1):
                 if dateparser.parse(f['published'],settings={'TO_TIMEZONE': 'UTC'}) >= latest:
                     msg = f'''{feed['feed']['title']} - {f['title']})\n\n{remove_tags(f['summary'])}\n\n{f['link']}'''
                     mastodon('post',msg)
-
+    
+    log(" ** All done **")
+    
 if __name__=='__main__':
     main('config.yaml')
