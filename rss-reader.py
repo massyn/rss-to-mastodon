ee41706e7706e994384256aa08f8c34760717f5a
Phil Massyn <phil.massyn@icloud.com>
2025-06-28 07:54:55 +1000

    Update

--- rss-reader.py
+++ rss-reader.py
@@ -4,34 +4,66 @@ import dateparser
 import datetime
 import re
 import os
+import logging
 from mastodon import Mastodon
+import socket
+from dotenv import load_dotenv
 
-def log(txt):
-    ts = datetime.datetime.now(datetime.timezone.utc).strftime('%Y-%m-%d %H:%M:%S')
-    print(f"{ts} - {txt}")
+load_dotenv()
+socket.setdefaulttimeout(10)          # seconds  ← change once, applies everywhere
+logging.basicConfig(
+    level="DEBUG",
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    datefmt="%Y-%m-%dT%H:%M:%SZ",
+)
 
-def remove_tags(text):
-    return re.compile(r'<[^>]+>').sub('', text)
+def read_rss(feed):
+    logging.info(f"Reading RSS feed {feed}")
+    try:
+        # Some sites are blocking us.  Not sure why -- it's RSS!  It's supposed to be scraped...
+        feed = feedparser.parse(feed, agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
+        logging.info(f" --> Found {len(feed.get('entries',[]))} items...")
+        return feed
+    except socket.timeout:
+        logging.warning(" --> timed out after 10s")
+    except Exception as exc:
+        logging.error(" --> failed: %s", exc)
+    return None
 
+def filter_rss(feed,latest):
+    data = []
+    for f in feed.get('entries',[]):
+        if 'published' in f and 'summary' in f:
+            published_date = dateparser.parse(f['published'],settings={'TIMEZONE': 'UTC', 'RETURN_AS_TIMEZONE_AWARE' : True})
+            if published_date > latest:
+                data.append({
+                    'feed_title'        : f['feed']['title'],
+                    'published_date'    : published_date,
+                    'title'             : f['title'],
+                    'summary'           : re.compile(r'<[^>]+>').sub('', f['summary']),
+                    'link'              : f['link']
+                })
+    return data
+            
 def main(c):
     # Read the configuration
     try:
         with open(c,'rt') as y:
             cfg = yaml.safe_load(y)        
     except:
-        log(f"Could not read {c} - ending...")
+        logging.error(f"Could not read {c} - ending...")
         exit(1)
 
     # Loop through each of the sections
     for section in cfg:
-        log(f"Section ==> {section}")
-        # -- we need our mastodon id, so lets set things up
+        logging.info(f"Section ==> {section}")
 
+        # -- we need our mastodon id, so lets set things up
         if not os.environ.get(f"{section}_ENDPOINT"):
-            log(f"ERROR - Environment variable {section}_ENDPOINT is not set.")
+            logging.error(f"Environment variable {section}_ENDPOINT is not set.")
             continue
         if not os.environ.get(f"{section}_ACCESS_TOKEN"):
-            log(f"ERROR - Environment variable {section}_ACCESS_TOKEN is not set.")
+            logging.error(f"Environment variable {section}_ACCESS_TOKEN is not set.")
             continue
 
         mastodon = Mastodon(
@@ -41,51 +73,33 @@ def main(c):
         try:
             me = mastodon.account_verify_credentials()
         except:
-            log("ERROR - something is wrong with the credentials.. Skipping...")
+            logging.error("Something is wrong with the credentials.. Skipping...")
             me = {}
-
+        me = {}
         if me.get('id'):
-            log(f" - id = {me['display_name']} ({me['id']})")
+            logging.info(f" - id = {me.get('display_name')} ({me.get('id')})")
             
             # -- Find the last time something was posted
-            status = mastodon.account_statuses(id=me['id'],limit=1)
+            status = mastodon.account_statuses(id=me.get('id'),limit=1)
             if len(status) == 0:
-                log(f" - {section} - Posting - 'Welcome to the RSS Bot'")
+                logging.info(f" - {section} - Posting - 'Welcome to the RSS Bot'")
                 mastodon.status_post('Welcome to the RSS Bot')
             else:
                 latest = status[0]['created_at']
-                log(f" - {section} - Latest timestamp from timeline is {latest}")
+                logging.info(f" - {section} - Latest timestamp from timeline is {latest}")
 
                 # -- read through each of the RSS feeds
                 for rss in cfg[section]:
-                    log(f" - {section} - rss => {rss}")
+                    logging.info(f" - {section} - rss => {rss}")
 
-                    try:
-                        # Some sites are blocking us.  Not sure why -- it's RSS!  It's supposed to be scraped...
-                        feed = feedparser.parse(rss, agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3')
-                    except:
-                        feed = {}
-                    log(f" - {section} -     => Found {len(feed.get('entries',[]))} items...")
+                    feed = read_rss(rss)
+                    content = filter_rss(feed,latest)
+                    for i in content:
+                        logging.info(f" - {section} -     => Posting - {i['feed_title']}")
+                        msg = f'''{i['feed_title']} - {i['title']})\n\n{f['summary']}\n\n{i['link']}'''
+                        mastodon.status_post(msg[:10000])
 
-                    # -- check each of the entries, and post if the item was published after our last status was sent.
-
-                    # TODO - add a label or tag to each post to indicate it was automated, so we only
-                    # check on the automated posted items. When posting manual messages, that could cause
-                    # the bot not to publish anything, since the timestamp has changed.
-                    for f in feed.get('entries',[]):
-                        if 'published' in f:
-                            published_date = dateparser.parse(f['published'],settings={'TIMEZONE': 'UTC', 'RETURN_AS_TIMEZONE_AWARE' : True})
-                            if published_date > latest:
-                                if 'summary' in f:
-                                    log(f" - {section} -     => Posting - {f['title']}")
-                                    msg = f'''{feed['feed']['title']} - {f['title']})\n\n{remove_tags(f['summary'])}\n\n{f['link']}'''
-                                    mastodon.status_post(msg[:10000])
-                                else:
-                                    log(f" -- something weird - we are missing summary, so we'll skip it")
-                        else:
-                            log(f" -- something weird - we are missing published, so we'll skip it")
-
-    log(" ** All done **")
+    logging.info(" ** All done **")
 
 if __name__=='__main__':
     main('config.yaml')
